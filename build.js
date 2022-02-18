b3c7d09b81284aaf2e9c7e7f3939d10f1703cf7e
Santiago Ruiz <sanruiz1003@gmail.com>
2022-02-18 13:33:38 -0500

    build

---
+++ build.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const packageJSON = JSON.parse(
+  fs.readFileSync(path.join(__dirname, './package.json'))
+);
+
+const packageLockJSON = JSON.parse(
+  fs.readFileSync(path.join(__dirname, './package-lock.json'))
+);
+
+const oldVersion = Number(packageJSON.version.replace(/\./g, ''));
+const version = String(oldVersion + 1)
+  .split('')
+  .join('.');
+
+const packageJSONnew = JSON.stringify({ ...packageJSON, version });
+fs.writeFileSync(path.join(__dirname, './package.json'), packageJSONnew);
+
+const packageLockJSONnew = JSON.stringify({
+  ...packageLockJSON,
+  version,
+  packages: {
+    ...packageLockJSON.packages,
+    '': { ...packageLockJSON.packages[''], version },
+  },
+});
+
+fs.writeFileSync(
+  path.join(__dirname, './package-lock.json'),
+  packageLockJSONnew
+);
