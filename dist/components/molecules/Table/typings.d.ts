b38f9e4abeff9b98a9f59f2e68ddfb9cf8e596aa
Santiago Ruiz <sanruiz1003@gmail.com>
2023-06-20 21:21:34 -0500

    bundle fix value table column

--- dist/components/molecules/Table/typings.d.ts
+++ dist/components/molecules/Table/typings.d.ts
@@ -2,7 +2,7 @@ import { TableProps as DefaultTableProps, TableColumnType } from 'antd';
 export { TablePaginationConfig, TableColumnType } from 'antd';
 export { FilterValue, SorterResult } from 'antd/lib/table/interface';
 export interface TableColumnProps<T> extends Omit<TableColumnType<T>, 'render'> {
-    render?: (value: string | number | boolean, record: T | TableRecord<T>) => JSX.Element | string | number | boolean | T;
+    render?: (value: string | number | boolean | Object, record: T | TableRecord<T>) => JSX.Element | string | number | boolean | T;
 }
 export interface TableProps<T = Record<string, any>> extends Omit<DefaultTableProps<T>, 'columns'> {
     columns: TableColumnProps<T>[];
